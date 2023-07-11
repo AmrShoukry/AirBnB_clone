3dd2d7de4f155f10c4f978dff4a95307eead2e95
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-07-11 07:25:11 +0300

    fixed some tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,5 @@
 import unittest
-import datetime
+from datetime import datetime
 from models.base_model import BaseModel
 """ Testing base model """
 
@@ -33,5 +33,5 @@ class TestBaseModel(unittest.TestCase):
     def test_save(self):
         """ Testing save(self) """
         old_update = self.my_model.updated_at
-        self.my_model.save(self)
+        self.my_model.save()
         self.assertNotEqual(old_update, self.my_model.updated_at)
