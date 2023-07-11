b1c1efbd9e6e5f070a568ba9abfc1c1b97ae7733
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-07-11 09:31:41 +0300

    update test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -9,29 +9,64 @@ class TestBaseModel(unittest.TestCase):
 
     def setUp(self):
         """ Setting up default initialization """
-        self.my_model = BaseModel()
+        self.my_model1 = BaseModel()
+        self.my_model2 = BaseModel(
+            id="1",
+            __class__="Amr",
+            created_at="2003-08-03T00:00:00",
+            updated_at="2023-07-11T00:00:00",
+            Age=20
+        )
 
     def test_init(self):
         """ Testing __init__(self) """
-        self.assertIsInstance(self.my_model.id, str)
-        self.assertIsInstance(self.my_model.created_at, datetime)
-        self.assertIsInstance(self.my_model.updated_at, datetime)
+        self.assertIsInstance(self.my_model1.id, str)
+        self.assertIsInstance(self.my_model1.created_at, datetime)
+        self.assertIsInstance(self.my_model1.updated_at, datetime)
+
+        self.assertIsInstance(self.my_model2.id, str)
+        self.assertIsInstance(self.my_model2.created_at, datetime)
+        self.assertIsInstance(self.my_model2.updated_at, datetime)
+        self.assertIsInstance(self.my_model2.Age, int)
+
+        # with self.assertRaises(ValueError):
+        #     BaseModel(
+        #         id = "1",
+        #         __class__ = "Amr",
+        #         created_at = "2003-08-03",
+        #         updated_at = "2023-07-11",
+        #         Age = 20
+        #     )
 
     def test_str(self):
         """ Testing __str__(self) """
-        id = self.my_model.id
-        expected_output = f"[BaseModel] ({id}) {self.my_model.__dict__}"
-        self.assertEqual(str(self.my_model), expected_output)
+        id1 = self.my_model1.id
+        expected_output1 = f"[BaseModel] ({id1}) {self.my_model1.__dict__}"
+        self.assertEqual(str(self.my_model1), expected_output1)
+
+        id2 = self.my_model2.id
+        expected_output2 = f"[BaseModel] ({id2}) {self.my_model2.__dict__}"
+        self.assertEqual(str(self.my_model2), expected_output2)
 
     def test_to_dict(self):
         """ Testing to_dict(self) """
-        expected_dic = self.my_model.to_dict()
+        expected_dic1 = self.my_model1.to_dict()
         keys = ["id", "created_at", "updated_at", "__class__"]
         for key in keys:
-            self.assertIn(key, expected_dic)
+            self.assertIn(key, expected_dic1)
+
+        # expected_dict2 = self.my_model2.to_dict()
+        # class_dict2 = {"__class__": "BaseModel"}
+        # keys2 = list(self.__dict__.keys().update({class_dict2}))
+        # for key in keys2:
+        #     self.assertIn(key, expected_dict2)
 
     def test_save(self):
         """ Testing save(self) """
-        old_update = self.my_model.updated_at
-        self.my_model.save()
-        self.assertNotEqual(old_update, self.my_model.updated_at)
+        old_update1 = self.my_model1.updated_at
+        self.my_model1.save()
+        self.assertNotEqual(old_update1, self.my_model1.updated_at)
+
+        old_update2 = self.my_model2.updated_at
+        self.my_model2.save()
+        self.assertNotEqual(old_update2, self.my_model2.updated_at)
