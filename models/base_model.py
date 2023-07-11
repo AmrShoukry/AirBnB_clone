4dbf60ae69cc0e3fcf40c71f24c6b6c2c55b16ef
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-07-11 07:10:26 +0300

    finished work on 4

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ every class inherits from this class
 """
 
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
@@ -14,15 +14,24 @@ class BaseModel:
     -is based off of this class
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         initializes the BaseModel
         id: random id using uuid4
         created_at, updated_at = datetime.now
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for arg in kwargs:
+                if arg == "__class__":
+                    continue
+                if arg == "created_at" or arg == "updated_at":
+                    vars(self)[arg] = datetime.fromisoformat(kwargs[arg])
+                else:
+                    vars(self)[arg] = kwargs[arg]
 
     def __str__(self):
         """
@@ -35,7 +44,7 @@ class BaseModel:
         """
         only modifies the updated_at variable
         """
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
