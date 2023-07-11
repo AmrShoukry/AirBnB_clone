51db04a0845b142dc8b17877926898d4ffa63d37
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-07-11 22:31:27 +0300

    6 - the console

--- console.py
+++ console.py
@@ -1,4 +1,25 @@
 #!/usr/bin/python3
+"""module that houses the console"""
+
 import cmd
 
 
+class HBNBCommand(cmd.Cmd):
+    """this is the console using the Cmd module"""
+    prompt = "(hbnb)"
+
+    def do_quit(self, line):
+        """quits the program"""
+        return True
+
+    def do_EOF(self, line):
+        """quits the program"""
+        return True
+
+    def emptyline(self):
+        """does nothing"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
