a68f6d056a54777b339ba5f5123b4267d022f12b
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-07-12 04:40:49 +0300

    update

--- console.py
+++ console.py
@@ -29,7 +29,29 @@ class HBNBCommand(cmd.Cmd):
         elif len(args) < 2:
             print("** instance id missing **")
             return True
-        return False
+        else:
+            search_key = '.'.join(args[:2])
+            search_result = storage.search(search_key)
+            if search_result is False:
+                print("** no instance found **")
+                return True
+        return search_key
+
+    def check_arg_validity_classes_attributes(self, args):
+        search_key = self.check_arg_validity_classes_ids(args)
+        if search_key is True:
+            return True
+        elif len(args) < 3:
+            print("** attribute name missing **")
+            return True
+        elif len(args) < 4:
+            print("** value missing **")
+            return True
+        elif args[2] == "id" or args[2] == "created_at" or args[2] == "updated_at":
+            return True
+        # elif not (type(args[2]) is str or type(args[2]) is float or type(args[2]) is int):
+        #     return True
+        return search_key, args[2], args[3]
 
     def do_quit(self, line):
         """quits the program"""
@@ -47,24 +69,16 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, line):
         args = line.split(" ")
-        if self.check_arg_validity_classes_ids(args):
-            pass
-        else:
-            search_key = '.'.join(args[:2])
+        search_key = self.check_arg_validity_classes_ids(args)
+        if search_key is not True:
             search_result = storage.search(search_key)
-            if search_result is False:
-                print("** no instance found **")
-            else:
-                print(search_result)
+            print(search_result)
 
     def do_destroy(self, line):
         args = line.split(" ")
-        if self.check_arg_validity_classes_ids(args):
-            pass
-        else:
-            search_key = '.'.join(args[:2])
-            if storage.destroy(search_key) is False:
-                print("** no instance found **")
+        search_key = self.check_arg_validity_classes_ids(args)
+        if search_key is not True:
+            storage.destroy(search_key)
 
     def do_all(self, line):
         args = line.split(" ")
@@ -72,6 +86,16 @@ class HBNBCommand(cmd.Cmd):
             result = storage.get_all_of_class(args[0])
             for class_instance in result:
                 print(class_instance)
+
+    def do_update(self, line):
+        args = line.split(" ")
+        search_key, key, value = self.check_arg_validity_classes_attributes(args)
+        search_instance = storage.search(search_key)
+        if search_key is not True:
+            # storage.destroy(search_key)
+            # storage.new_and_update(search_key)
+            pass
+
 
     def do_EOF(self, line):
         """quits the program"""
