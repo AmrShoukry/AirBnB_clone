7a2132f4e4c681630903d17c3cc39f7de459d1f3
ahmedAhmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-07-12 09:11:13 +0300

    Create user.py

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module that houses the user class"""
+
+from base_model import BaseModel
+
+class User(BaseModel):
+    """this is the class for our users"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
