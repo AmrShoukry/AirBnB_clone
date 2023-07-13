8d751590d83115a2e899a5258afc12386d7b6bdf
ahmedAhmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-07-14 01:54:08 +0300

    pep8 codestyle and comments

--- console.py
+++ console.py
@@ -29,6 +29,7 @@ class HBNBCommand(cmd.Cmd):
     }
 
     def check_arg_validity_classes(self, args):
+        """checks if the class is a real class and that it was given in args"""
         if len(args) < 1 or args[0] == '':
             print("** class name missing **")
             return True
@@ -38,6 +39,7 @@ class HBNBCommand(cmd.Cmd):
         return False
 
     def check_arg_validity_classes_ids(self, args):
+        """checks all as previous with extra check for id"""
         if self.check_arg_validity_classes(args):
             return True
         elif len(args) < 2:
@@ -52,6 +54,7 @@ class HBNBCommand(cmd.Cmd):
         return search_key
 
     def check_arg_validity_classes_attributes(self, args):
+        """checks all as previous with extra check for attributes as dict"""
         search_key = self.check_arg_validity_classes_ids(args)
         if search_key is True:
             return True
@@ -70,6 +73,7 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_create(self, line):
+        """creates a class instance"""
         args = line.split(" ")
         if self.check_arg_validity_classes(args):
             pass
@@ -80,6 +84,7 @@ class HBNBCommand(cmd.Cmd):
             storage.save()
 
     def do_show(self, line):
+        """shows an instance given a classname and id"""
         args = line.split(" ")
         search_key = self.check_arg_validity_classes_ids(args)
         if search_key is not True:
@@ -87,18 +92,21 @@ class HBNBCommand(cmd.Cmd):
             print(search_result)
 
     def do_destroy(self, line):
+        """destroys a class give classname and id"""
         args = line.split(" ")
         search_key = self.check_arg_validity_classes_ids(args)
         if search_key is not True:
             storage.destroy(search_key)
 
     def do_all(self, line):
+        """shows all of a certain class or all classes if no extra arg"""
         args = line.split(" ")
         if args[0] == "" or self.check_arg_validity_classes(args) is False:
             result = storage.get_all_of_class(args[0])
             print(result)
 
     def do_update(self, line):
+        """updates the class args[0] with the variable args[1]"""
         args = line.split(" ")
         search_key = self.check_arg_validity_classes_attributes(args)
         if search_key is not True:
@@ -127,8 +135,6 @@ class HBNBCommand(cmd.Cmd):
             
             storage.update_new(created_class, id, created_at, updated_at, args[2], args[3])                
             storage.save()
-
-
 
     def do_EOF(self, line):
         """quits the program"""
