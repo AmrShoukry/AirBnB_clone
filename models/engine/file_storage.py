404e3c76684338749d5485895e9846e5b573c221
ahmedAhmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-07-15 13:50:51 +0300

    removed unncessary functions for 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,17 +16,6 @@ class FileStorage:
         """ returns the dictionary """
         return self.__objects
 
-    def update_new(self, obj, search_dict, created_at, updated_at, key, value):
-        """creates a new instance of a class as a means of updating it"""
-        setattr(obj, key, value)
-        obj.created_at = created_at
-        obj.updated_at = updated_at
-        for dict_key, dict_value in search_dict.items():
-            if dict_key == "__class__":
-                continue
-            setattr(obj, dict_key, dict_value)
-        self.new(obj)
-
     def new(self, obj):
         """ sets in __objects the obj with key <obj class name>.id """
         self.__objects[f"{type(obj).__name__}.{obj.id}"] = obj
@@ -56,33 +45,3 @@ class FileStorage:
                 for key in object_list.keys():
                     value = object_list[key]
                     self.__objects[key] = eval(value["__class__"])(**value)
-
-    def search(self, key):
-        """searches for a class given a key of classname.id"""
-        if key in self.__objects:
-            return self.__objects[key]
-        return False
-
-    def destroy(self, key):
-        """destroys an instance of a class given classname and id"""
-        if key in self.__objects:
-            del self.__objects[key]
-            self.save()
-            return True
-        return False
-
-    def get_all_of_class(self, class_name):
-        """shows all of a certain class, or all if no class is given"""
-        result = []
-        for key in self.__objects.keys():
-            if key.startswith(class_name):
-                result.append(self.__objects[key])
-        return result
-
-    def count_class(self, class_name):
-        """Count number of classes of a specific type"""
-        count = 0
-        for key in self.__objects.keys():
-            if key.startswith(class_name):
-                count += 1
-        return count
