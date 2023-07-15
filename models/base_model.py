256384f77e7eb5faba104e7c1532266e0cecfee2
ahmedAhmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-07-15 14:31:54 +0300

    better implementation of basemodel init

--- models/base_model.py
+++ models/base_model.py
@@ -23,8 +23,9 @@ class BaseModel:
         """
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            time_created = datetime.now()
+            self.created_at = time_created
+            self.updated_at = time_created
             models.storage.new(self)
         else:
             for arg in kwargs:
