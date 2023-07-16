a9030047228f820774e586b22bf45e15566683a1
ahmedAhmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-07-16 08:03:32 +0300

    skeleton for all tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -12,8 +12,31 @@ class TestConsole(unittest.TestCase):
         self.storage_tester = FileStorage()
 
     def test_all(self):
-        """ Testing __init__(self) """
+        """ Testing all(self) """
         self.assertEqual(self.storage_tester.all(), {})
+
+    def test_new(self):
+        """ Testing new(self) """
+        self.assertEqual(1, 1)
+
+    def test_reload(self):
+        """ Testing save(self) """
+        self.assertEqual(1, 1)
+
+    def test_search(self):
+        """ Testing save(self) """
+        self.assertEqual(1, 1)
+
+    def test_destroy(self):
+        """ Testing save(self) """
+        self.assertEqual(1, 1)
+    def test_get_all_of_class(self):
+        """ Testing save(self) """
+        self.assertEqual(1, 1)
+
+    def test_count_class(self):
+        """ Testing save(self) """
+        self.assertEqual(1, 1)
 
 
 if __name__ == '__main__':
